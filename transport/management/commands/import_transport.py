d106f8ca0b6726e2ed50795026ae7a4b5d9c8dc8
xi455 <a3083493@gmail.com>
2024-06-12 20:33:47 +0800

    refactor: import csv file code

--- transport/management/commands/import_transport.py
+++ transport/management/commands/import_transport.py
@@ -1,5 +1,5 @@
-import csv
 import os
+import pandas as pd
 
 from django.core.management.base import BaseCommand
 
@@ -11,26 +11,28 @@ class Command(BaseCommand):
     help = "Import data from a CSV file"
 
     def import_data(self, file_path, model, foreign_keys=None):
-        with open(file_path, newline="") as csvfile:
-            reader = csv.DictReader(csvfile)
-            print(
-                f"Waiting for data to be imported from {os.path.basename(file_path)}..."
-            )
-            for row in reader:
-                print(row)
-                if foreign_keys:
-                    for key, related_model in foreign_keys.items():
-                        if row[key]:
-                            row[key] = related_model.objects.get(id=row[key])
-                        else:
-                            row[key] = None
 
-                model.objects.create(**row)
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Successfully imported data from {os.path.basename(file_path)}"
-                )
+        df = pd.read_csv(file_path)
+        batch_size = 1000  # 調整批次大小
+
+        objects = list()
+        for _, row in df.iterrows():
+            print(row)
+            if foreign_keys:
+                for key, related_model in foreign_keys.items():
+                    if row[key]:
+                        row[key] = related_model.objects.get(id=row[key])
+                    else:
+                        row[key] = None
+
+        objects.append(model(**row))
+        model.objects.bulk_create(objects, batch_size=batch_size)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully imported data from {os.path.basename(file_path)}"
             )
+        )
 
     def handle(self, *args, **kwargs):
         base_path = os.path.join(
